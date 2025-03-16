68ba343734bb5113001fbd0f526191be6a9b9c00
HadeerAlaa542 <hadeeralaa542@gmail.com>
2025-03-16 03:05:18 +0200

    Delete jenkins/jenkins-shared-library/vars directory

--- jenkins/jenkins-shared-library/vars/BuildandPushDockerImage.groovy
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env groovy
-def call(String dockerHubCredentialsID, String repoName, String imageName) {
-	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        	sh "docker build -t ${repoName}/${imageName}:${BUILD_NUMBER} ."
-        	sh "docker push ${repoName}/${imageName}:${BUILD_NUMBER}"	 
-        	sh "docker rmi ${repoName}/${imageName}:${BUILD_NUMBER}"
-  }
-}
