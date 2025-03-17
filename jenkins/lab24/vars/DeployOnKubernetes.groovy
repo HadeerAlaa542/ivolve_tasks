63a4d2d55337869214163367f64a050f7a17b435
HadeerAlaa542 <hadeeralaa542@gmail.com>
2025-03-18 01:57:52 +0200

    Update DeployOnKubernetes.groovy

--- jenkins/lab24/vars/DeployOnKubernetes.groovy
+++ jenkins/lab24/vars/DeployOnKubernetes.groovy
@@ -2,8 +2,12 @@
 def call(String k8sCredentialsID, String repoName, String imageName, String deploymentFile, String namespace) {
     def tag = env.BRANCH_NAME   // Use branch name as tag
     withCredentials([file(credentialsId: "${k8sCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
-    sh "sed -i 's|image:.*|image: ${repoName}/${imageName}:${tag}|g' ${deploymentFile}"    
-    sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f ${deploymentFile} -n ${namespace}"
+          sh """
+            sed -i 's|image:.*|image: ${repoName}/${imageName}:${tag}|g' ${deploymentFile}
+            sed -i 's|namespace:.*|namespace: ${namespace}|g' ${deploymentFile}
+            export KUBECONFIG=\$KUBECONFIG_FILE
+            kubectl apply -f ${deploymentFile} -n ${namespace}
+        """
     }
 }
 
