7feac2ed0f5e92b8d9074699383ae52ed7e93775
HadeerAlaa542 <hadeeralaa542@gmail.com>
2025-03-18 04:22:56 +0200

    Update BuildandPushDockerimage.groovy

--- jenkins/lab24/vars/BuildandPushDockerimage.groovy
+++ jenkins/lab24/vars/BuildandPushDockerimage.groovy
@@ -2,8 +2,8 @@
 def call(String dockerHubCredentialsID, String repoName, String imageName) {
 	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
 		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        	sh "docker build -t ${repoName}/${imageName}:${BUILD_NUMBER} ."
-        	sh "docker push ${repoName}/${imageName}:${BUILD_NUMBER}"	 
+        	sh " docker build -t ${repoName}/${imageName}:${env.BRANCH_NAME} ."
+        	sh "docker push ${repoName}/${imageName}:${env.BRANCH_NAME}"	 
         	sh "docker rmi ${repoName}/${imageName}:${BUILD_NUMBER}"
   }
 }
