e49c640b1f4dc79ac731423bed183f42f894c764
HadeerAlaa542 <hadeeralaa542@gmail.com>
2025-03-18 04:40:45 +0200

    Update BuildandPushDockerimage.groovy

--- jenkins/lab24/vars/BuildandPushDockerimage.groovy
+++ jenkins/lab24/vars/BuildandPushDockerimage.groovy
@@ -1,9 +1,9 @@
-#!usr/bin/env groovy
+#!/usr/bin/env groovy
 def call(String dockerHubCredentialsID, String repoName, String imageName) {
-	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        	sh " docker build -t ${repoName}/${imageName}:${env.BRANCH_NAME} ."
-        	sh "docker push ${repoName}/${imageName}:${env.BRANCH_NAME}"	 
-        	sh "docker rmi ${repoName}/${imageName}:${BUILD_NUMBER}"
-  }
+    withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+        sh "docker build -t ${repoName}/${imageName}:${env.BRANCH_NAME} ."  // Use branch name for consistency
+        sh "docker push ${repoName}/${imageName}:${env.BRANCH_NAME}"	 
+        sh "docker rmi ${repoName}/${imageName}:${env.BRANCH_NAME}"
+    }
 }
